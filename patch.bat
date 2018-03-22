efd4a6b7402e83de42aff3f34e3e52a7247fa1ce
dcLunatic <dc_lunatic@qq.com>
2018-03-22 15:31:08 +0800

    Add files via upload

---
+++ patch.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if not "%1"=="" goto :crack
+echo 请将libTDMaster.dll拖动到该批处理文件
+pause>nul
+goto :end
+
+
+
+:crack
+taskkill /f /t /im StudentMain.exe
+taskkill /f /t /im PhantosysClientService.exe
+taskkill /f /t /im MasterHelper.exe
+
+del /f "C:\Program Files (x86)\Phantosys\PhantosysCloud v5.0 2014 standard\libTDMaster.dll"
+move %1 "C:\Program Files (x86)\Phantosys\PhantosysCloud v5.0 2014 standard\libTDMaster.dll"
+
+"C:\Program Files (x86)\Phantosys\PhantosysCloud v5.0 2014 standard\StudentMain.exe"
+echo 上面如果成功移动文件，则表示成功了。
+ping 127.0.0.1 > nul
+:end
